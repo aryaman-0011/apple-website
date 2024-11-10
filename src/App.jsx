c56cf8a3724edf060d12e691b8d39b21316a0503
Aryaman Kushwaha <kushwaha.aryamamn21@gmail.com>
2024-11-10 16:54:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./coponents/Navbar";
+import Hero from "./coponents/Hero";
+import Highlights from "./coponents/Highlights";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App;
